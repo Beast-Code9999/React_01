1cd0d90588adfd3dfcb5b6d8a2d083d55b57fcf7
Jason_Lay <beastcode9999@gmail.com>
2024-05-01 20:08:35 +0930

    add Footer, Header, Menu components

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -50,12 +50,32 @@ const pizzaData = [
 function App() {
     return (
         <div>
-            <h1>Hello World</h1>
-            <Pizza />
-            <Pizza />
-            <Pizza />
+          <Header />
+          <Menu />
+          <Footer />
         </div>
     )    
+}
+
+function Header() {
+  return <h1>Fast React Pizza Company</h1>
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  )
+}
+
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()}. We are currently open </footer>
+  
+  // return React.createElement('footer', null, "We are currently open")
 }
 
 function Pizza() {
