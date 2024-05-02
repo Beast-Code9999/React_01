f11dcdc49dccefc46bba305d1456a3369f3d935c
Jason_Lay <beastcode9999@gmail.com>
2024-05-02 20:40:48 +0930

    add Components in profile-card (not specific i know)

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -6,17 +6,49 @@ function App() {
   return (
     <div className='card'>
       <Avatar />
-      <div>
-        
+      <div className='data'>
+        <Intro />
+        <Skillset />
       </div>
     </div>
   )
 } 
 
 function Avatar() {
-
+  return 
 }
 
+function Intro() {
+  const name = "Jason Lay"
+  const description = "Full-stack web developer and student at Charles Darwin University. When I am not coding I like to spend time with my family and listen to music"
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+function Skillset() {
+  return (
+    <div className='skill-list'>
+      <Skill skill="React" emoji="💪" color="#58c4dc"/>
+      <Skill skill="HTML" emoji="💪"/>
+      <Skill skill="CSS" emoji="💪"/>
+      <Skill skill="JavaScript" emoji="💪"/>
+    </div>
+  )
+}
+
+function Skill(props) {
+  return (
+    <div className='skill'>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
