38fef67e288c6cb3e48a7bd3ef64c5a73613fe41
Jason_Lay <beastcode9999@gmail.com>
2024-05-02 20:49:40 +0930

    finish the profile-card practice (minus the img)

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -1,57 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
-    <div className='card'>
+    <div className="card">
       <Avatar />
-      <div className='data'>
+      <div className="data">
         <Intro />
         <Skillset />
       </div>
     </div>
-  )
-} 
+  );
+}
 
 function Avatar() {
-  return 
+  return;
 }
 
 function Intro() {
-  const name = "Jason Lay"
-  const description = "Full-stack web developer and student at Charles Darwin University. When I am not coding I like to spend time with my family and listen to music"
+  const name = "Jason Lay";
+  const description =
+    "Full-stack web developer and student at Charles Darwin University. When I am not coding I like to spend time with my family and listen to music";
 
   return (
-    <div>
+    <div className="data">
       <h1>{name}</h1>
       <p>{description}</p>
     </div>
-  )
+  );
 }
 
 function Skillset() {
   return (
-    <div className='skill-list'>
-      <Skill skill="React" emoji="💪" color="#58c4dc"/>
-      <Skill skill="HTML" emoji="💪" color="#c45534"/>
-      <Skill skill="CSS" emoji="💪" color="#3f4dd4"/>
-      <Skill skill="JavaScript" emoji="💪" color="#e8d850"/>
+    <div className="skill-list data">
+      <Skill skill="React" emoji="💪" color="#58c4dc" />
+      <Skill skill="HTML" emoji="💪" color="#c45534" />
+      <Skill skill="CSS" emoji="💪" color="#3f4dd4" />
+      <Skill skill="JavaScript" emoji="💪" color="#e8d850" />
     </div>
-  )
+  );
 }
 
 function Skill(props) {
   return (
-    <div className='skill'>
+    <div className="skill" style={{ backgroundColor: props.color }}>
       <span>{props.skill}</span>
       <span>{props.emoji}</span>
     </div>
-  )
+  );
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
