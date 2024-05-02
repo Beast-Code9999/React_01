b0e289cbfdaab8de46c72202419c9ab36cc95526
Jason_Lay <beastcode9999@gmail.com>
2024-05-02 21:18:01 +0930

    add conditional rendering using (isOpen && someRendering) in Footer component

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -116,7 +116,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We are currently open 
+      {isOpen && (
+        <div className="order">
+          <p>We are open until {closeHour}pm. Come visit us when you are craving a pizza or two</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   )
   // return React.createElement('footer', null, "We are currently open")
