6a59e9d24b6c436681fe063a9ffbfef770c636b6
Jason_Lay <beastcode9999@gmail.com>
2024-05-02 21:23:27 +0930

    adding conditional rendering using "&&" falsy values won't render in Map() component

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -69,16 +69,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData
+  const numPizzas = pizzas.length
+
   return (
     <main  className="menu">
       <h2>Our menu</h2>
-
-      <ul className="pizzas">
+      {numPizzas > 0 && (
+        <ul className="pizzas">
         {/* very important technique below */}
         {pizzaData.map(pizza => (
           <Pizza pizzaObj={pizza} key={pizza.name}/>
         ))}
       </ul>
+      )}
+
       {/* <Pizza name="Pizza Spinaci" 
       ingredients='Tomato, mozarella, spinach, and ricotta cheese' 
       photoName="pizzas/spinaci.jpg" 
